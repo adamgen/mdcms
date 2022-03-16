bff0417336d9b3fe9d0d7f0155d28811f33c1b0d
Adam <adamgensh@gmail.com>
2022-03-16 17:31:00 +0200

    nx g react app

---
+++ apps/md-cms/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  displayName: 'md-cms',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      { jsc: { transform: { react: { runtime: 'automatic' } } } },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/md-cms',
+};
