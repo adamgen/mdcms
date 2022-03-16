b70d20142fecd0e991b7bf9de784715b851dbbb6
Adam <adamgensh@gmail.com>
2022-03-16 21:47:33 +0200

    Add basic test to drawer-toggler

--- apps/md-cms/src/app/app-layout/app-layout.spec.tsx
+++ apps/md-cms/src/app/app-layout/app-layout.spec.tsx
@@ -1,13 +1,22 @@
 import { fireEvent, screen } from '@testing-library/react';
 
 import { AppLayout } from './app-layout';
-import {render} from '../../test-utils';
+import { render } from '../../test-utils';
+import { configureStore } from '@reduxjs/toolkit';
+import { appReducer } from '../../root-state';
 
 describe('AppLayout', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<AppLayout />);
+    const store = configureStore({ reducer: appReducer });
+    const { baseElement } = render(<AppLayout />, {
+      store,
+    });
     expect(baseElement).toBeTruthy();
 
+    expect(store.getState().drawer.isOpen).toBe(true);
     fireEvent.click(screen.getByTestId('drawer-toggler'));
+    expect(store.getState().drawer.isOpen).toBe(false);
+    fireEvent.click(screen.getByTestId('drawer-toggler'));
+    expect(store.getState().drawer.isOpen).toBe(true);
   });
 });
