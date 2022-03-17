877d6d3ca2509014ce7a0cd9eadf356149d5aa12
Adam <adamgensh@gmail.com>
2022-03-17 15:08:00 +0200

    Test files api store update

--- apps/md-cms/src/app/app-layout/app-drawer/fs-tree/fs-tree.slice.spec.ts
+++ apps/md-cms/src/app/app-layout/app-drawer/fs-tree/fs-tree.slice.spec.ts
@@ -1,16 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filesApi } from './fs-tree.slice';
-import fetchMock from "jest-fetch-mock"
+import fetchMock from 'jest-fetch-mock';
 import { appReducer } from '../../../../root-state';
+
+const makeStore = () =>
+  configureStore({
+    reducer: appReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(filesApi.middleware),
+  });
 
 describe('fsTree reducer', () => {
   it('should handle initial state', async () => {
-    const store = configureStore({
-      reducer: appReducer,
-      middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(filesApi.middleware),
-    });
-
+    const store = makeStore();
     await store.dispatch(filesApi.endpoints.getFileByName.initiate('index.md'));
     expect(fetch).toBeCalledTimes(1);
     const { method, headers, url } = fetchMock.mock.calls[0][0] as Request;
@@ -22,5 +24,20 @@ describe('fsTree reducer', () => {
     expect(url).toBe('http://localhost:3002/files/index.md');
     expect(accept).toBe('application/json');
     expect(authorization).toBeNull();
+  });
+
+  it('should store server response to state', async () => {
+    const store = makeStore();
+    fetchMock.mockResponse(JSON.stringify(''));
+
+    await store.dispatch(filesApi.endpoints.getFileByName.initiate('index.md'));
+    const request =
+      store.getState()[filesApi.reducerPath].queries[
+        'getFileByName("index.md")'
+      ]!;
+
+    expect(request).toBeTruthy();
+
+    expect(request.data).toBe('');
   });
 });
