2b6e1bb6205b84ff0d9b85503f88c7cac343c8de
Adam <adamgensh@gmail.com>
2022-03-18 15:53:40 +0200

    Replace literals with consts

--- apps/md-cms/src/app/app-layout/app-drawer/fs-tree/fs-tree.slice.spec.tsx
+++ apps/md-cms/src/app/app-layout/app-drawer/fs-tree/fs-tree.slice.spec.tsx
@@ -21,10 +21,17 @@ const wrapper: React.FC = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
 };
 
+const indexMd = 'index.md';
+const blogMd = 'blog.md';
+const filesMock = {
+  [indexMd]: `# This is the index file`,
+  [blogMd]: `# This is the blog`,
+};
+
 describe('fsTree reducer', () => {
-  it('should handle initial state', async () => {
+  it('should call fetch when called', async () => {
     const store = makeStore();
-    await store.dispatch(filesApi.endpoints.getFileByName.initiate('index.md'));
+    await store.dispatch(filesApi.endpoints.getFileByName.initiate(indexMd));
     expect(fetch).toBeCalledTimes(1);
     const { method, headers, url } = fetchMock.mock.calls[0][0] as Request;
 
@@ -32,31 +39,31 @@ describe('fsTree reducer', () => {
     const authorization = headers.get('authorization');
 
     expect(method).toBe('GET');
-    expect(url).toBe(`${BACKEND_URL}/files/index.md`);
+    expect(url).toBe(`${BACKEND_URL}/files/${indexMd}`);
     expect(accept).toBe('application/json');
     expect(authorization).toBeNull();
   });
 
   it('should store server response to redux state', async () => {
     const store = makeStore();
-    fetchMock.mockResponse(JSON.stringify(''));
+    fetchMock.mockResponse(JSON.stringify(filesMock[indexMd]));
 
-    await store.dispatch(filesApi.endpoints.getFileByName.initiate('index.md'));
+    await store.dispatch(filesApi.endpoints.getFileByName.initiate(indexMd));
     const request =
       store.getState()[filesApi.reducerPath].queries[
-        'getFileByName("index.md")'
+        `getFileByName("${indexMd}")`
       ]!;
 
     expect(request).toBeTruthy();
 
-    expect(request.data).toBe('');
+    expect(request.data).toBe(filesMock[indexMd]);
   });
 
   it('should request the right URL from a hook', async () => {
     const promise = Promise.resolve();
-    fetchMock.mockResponse(JSON.stringify(''));
+    fetchMock.mockResponse(JSON.stringify(filesMock[indexMd]));
     const { waitForNextUpdate } = renderHook(
-      () => useGetFileByNameQuery('index.md'),
+      () => useGetFileByNameQuery(indexMd),
       {
         wrapper,
       }
@@ -65,23 +72,23 @@ describe('fsTree reducer', () => {
 
     const { url } = fetchMock.mock.calls[2][0] as Request;
 
-    expect(url).toBe(`${BACKEND_URL}/files/index.md`);
+    expect(url).toBe(`${BACKEND_URL}/files/${indexMd}`);
 
     await act(() => promise);
   });
 
   it('should return server response from a hook', async () => {
     const promise = Promise.resolve();
-    fetchMock.mockResponse(JSON.stringify(''));
+    fetchMock.mockResponse(JSON.stringify(filesMock[indexMd]));
     const { result, waitForNextUpdate } = renderHook(
-      () => useGetFileByNameQuery('index.md'),
+      () => useGetFileByNameQuery(indexMd),
       {
         wrapper,
       }
     );
     await waitForNextUpdate();
 
-    expect(result.current.data).toBe('');
+    expect(result.current.data).toBe(filesMock[indexMd]);
 
     await act(() => promise);
   });
