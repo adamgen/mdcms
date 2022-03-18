a823b8622ff66bb50e5e40c70e0ccb9a965fb776
Adam <adamgensh@gmail.com>
2022-03-18 15:44:48 +0200

    Build getBackendUrl

--- libs/shared-env/src/lib/shared-env.ts
+++ libs/shared-env/src/lib/shared-env.ts
@@ -1,3 +1,11 @@
-export function sharedEnv(): string {
-  return 'shared-env';
+export function getBackendUrl(): string {
+  const port = process.env['MD_SERVER_PORT'];
+  const baseUrl = process.env['MD_SERVER_BASE_URL'];
+  if (!port) {
+    throw new Error('Env var MD_SERVER_PORT is missing.');
+  }
+  if (!baseUrl) {
+    throw new Error('Env var MD_SERVER_BASE_URL is missing.');
+  }
+  return port === '80' ? baseUrl : `${baseUrl}:${port}`;
 }
