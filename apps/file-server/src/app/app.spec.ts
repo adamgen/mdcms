bf2f982f17363a3421c3be866d75b887dcc00745
Adam <adamgensh@gmail.com>
2022-03-20 16:37:25 +0200

    Add tests to app.spec.ts

--- apps/file-server/src/app/app.spec.ts
+++ apps/file-server/src/app/app.spec.ts
@@ -1,9 +1,24 @@
 import * as request from 'supertest';
 import { app } from './app';
 
+const testRoute = async (
+  route: string,
+  test: (response: request.Response) => unknown
+) => {
+  const response = await request(app).get(route);
+  test(response);
+};
+
 describe('app health', () => {
   it('should respond with 404 for non existing route', async () => {
-    const response = await request(app).get('/non-existing-route');
-    expect(response.statusCode).toBe(404);
+    await testRoute('/non-existing-route', (response) => {
+      expect(response.statusCode).toBe(404);
+    });
+    await testRoute('/api', (response) => {
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toBe(
+        JSON.stringify({ message: 'Welcome to file-server!' })
+      );
+    });
   });
 });
