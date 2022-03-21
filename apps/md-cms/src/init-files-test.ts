fa2a1bdfa13025b2d570f549578b1b82011c978f
Adam <adamgensh@gmail.com>
2022-03-21 14:25:30 +0200

    Upgrade init-files-test.ts

--- apps/md-cms/src/init-files-test.ts
+++ apps/md-cms/src/init-files-test.ts
@@ -3,12 +3,15 @@
 import * as path from 'path';
 import * as fs from 'fs-extra';
 
-export const initFilesTest = (dir: string)=>{
+export const initFilesTest = (dir: string) => {
   let env = process.env;
   const tempTestDir = path.join(dir, '__test-temp-data__');
   beforeEach(() => {
     env = process.env;
     process.env['FILES_SERVER_BASE_PATH'] = tempTestDir;
+    if (fs.existsSync(tempTestDir)) {
+      fs.removeSync(tempTestDir);
+    }
     fs.copySync(path.join(dir, '__testing-files__'), tempTestDir);
   });
 
@@ -16,4 +19,4 @@ export const initFilesTest = (dir: string)=>{
     fs.removeSync(tempTestDir);
     process.env = env;
   });
-}
+};
