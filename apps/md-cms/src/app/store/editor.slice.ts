8e005240e6c7f8dd903ba32a18ca26a9a41a5c61
Adam <adamgensh@gmail.com>
2022-03-26 01:10:15 +0300

    Remove unused imports

--- apps/md-cms/src/app/store/editor.slice.ts
+++ apps/md-cms/src/app/store/editor.slice.ts
@@ -1,4 +1,4 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../root-state';
 
 export const EDITOR_FEATURE_KEY = 'editor';
