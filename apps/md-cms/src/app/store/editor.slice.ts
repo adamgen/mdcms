e1ac7ad700ec3baa93a30dc0090843a21d258916
Adam <adamgensh@gmail.com>
2022-03-27 18:12:48 +0300

    Update store updater to accept all args

--- apps/md-cms/src/app/store/editor.slice.ts
+++ apps/md-cms/src/app/store/editor.slice.ts
@@ -3,7 +3,7 @@ import { RootState } from '../../root-state';
 
 export const EDITOR_FEATURE_KEY = 'editor';
 
-interface File {
+export interface File {
   path: string;
   content: string;
 }
@@ -21,24 +21,33 @@ export const initialEditorState: EditorState = {
   path: null,
   localContent: '',
   selectedFilePath: '',
+  selectedFile: undefined,
+  newFile: undefined,
 };
+
+const storeAllowedKeys = Object.keys(initialEditorState);
+type TStoreAllowedKeys = (keyof EditorState)[];
 
 export const editorSlice = createSlice({
   name: EDITOR_FEATURE_KEY,
   initialState: initialEditorState,
   reducers: {
     update: (state, action: PayloadAction<Partial<EditorState>>) => {
-      state.path = action.payload.path ?? state.path;
-      state.localContent = action.payload.localContent ?? state.localContent;
-      state.selectedFilePath =
-        action.payload.selectedFilePath ?? state.selectedFilePath;
+      (Object.keys(action.payload) as TStoreAllowedKeys)
+        .filter((key) => storeAllowedKeys.includes(key))
+        .forEach((key) => {
+          const value = action.payload[key];
+          if (!value) {
+            return;
+          }
+          // @ts-ignore
+          state[key] = value;
+        });
     },
   },
 });
 
 export const editorReducer = editorSlice.reducer;
-
-export const editorActions = editorSlice.actions;
 
 export const getEditorState = (rootState: RootState): EditorState =>
   rootState[EDITOR_FEATURE_KEY];
