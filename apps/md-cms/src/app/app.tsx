8cdebc885130275c39043e00648d0e0556aa3c6c
Adam <adamgensh@gmail.com>
2022-03-27 20:30:49 +0300

    Init app routing

--- apps/md-cms/src/app/app.tsx
+++ apps/md-cms/src/app/app.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
+import { Route } from 'react-router-dom';
 
 import { AppLayout } from './app-layout/app-layout';
 import MdEditor from './components/md-editor/md-editor';
 import FilePathTitle from './components/file-path-title/file-path-title';
+import EmptyView from './views/empty-view/empty-view';
 
 const StyledApp = styled.div``;
 
@@ -10,8 +12,13 @@ export function App() {
   return (
     <StyledApp>
       <AppLayout>
-        <FilePathTitle />
-        <MdEditor />
+        <Route path={'/'} exact={true}>
+          <EmptyView />
+        </Route>
+        <Route path={'/file'}>
+          <FilePathTitle />
+          <MdEditor />
+        </Route>
       </AppLayout>
     </StyledApp>
   );
--- apps/md-cms/src/app/components/fs-tree/fs-tree.tsx
+++ apps/md-cms/src/app/components/fs-tree/fs-tree.tsx
@@ -3,12 +3,31 @@ import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import TreeItem from '@mui/lab/TreeItem';
-import { useGetFilesListQuery } from '../../store/files.api';
 import { useDispatch } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+import { useGetFilesListQuery } from '../../store/files.api';
 import { editorSlice } from '../../store/editor.slice';
+
+function useQuery(param: string) {
+  const { search } = useLocation();
+
+  return (
+    React.useMemo(() => new URLSearchParams(search), [search]).get(param) ??
+    undefined
+  );
+}
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #000;
+`;
 
 export function FsTree() {
   const { isLoading, data } = useGetFilesListQuery();
+  const name = useQuery('name');
+
   const dispatch = useDispatch();
 
   if (isLoading) {
@@ -26,22 +45,26 @@ export function FsTree() {
       defaultExpandIcon={<ChevronRightIcon />}
       sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
       data-testid="fs-tree"
+      defaultSelected={name}
+      multiSelect={false}
     >
       {data.map((filename) => (
-        <TreeItem
-          key={filename}
-          onClick={() => {
-            dispatch(
-              editorSlice.actions.update({
-                selectedFilePath: filename,
-                path: filename,
-              })
-            );
-          }}
-          nodeId={filename}
-          label={filename}
-          data-testid={`file-name--${filename}`}
-        />
+        <StyledLink key={filename} to={`/file?name=${filename}`}>
+          <TreeItem
+            className={'Mui-selected'}
+            onClick={() => {
+              dispatch(
+                editorSlice.actions.update({
+                  selectedFilePath: filename,
+                  path: filename,
+                })
+              );
+            }}
+            nodeId={filename}
+            label={filename}
+            data-testid={`file-name--${filename}`}
+          />
+        </StyledLink>
       ))}
     </TreeView>
   );
