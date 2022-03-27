25add61e04df5654ab567003332f51847df601fc
Adam <adamgensh@gmail.com>
2022-03-27 23:00:25 +0300

    Remove avatar icon to prevent 404

--- apps/md-cms/src/app/app-layout/app-navigation/app-navigation.tsx
+++ apps/md-cms/src/app/app-layout/app-navigation/app-navigation.tsx
@@ -68,7 +68,7 @@ export const AppNavigation = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Adamgen" />
               </IconButton>
             </Tooltip>
             <Menu
