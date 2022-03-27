92a5ef59485aa39cd61c793d9542bf694dbf2e68
Adam <adamgensh@gmail.com>
2022-03-27 23:09:24 +0300

    Basic activate new-file-view.tsx

--- apps/md-cms/src/app/views/new-file-view/new-file-view.tsx
+++ apps/md-cms/src/app/views/new-file-view/new-file-view.tsx
@@ -1,16 +1,66 @@
 import styled from '@emotion/styled';
+import FilePathTitle from '../../components/file-path-title/file-path-title';
+import MdEditor from '../../components/md-editor/md-editor';
+import { useMemo, useState } from 'react';
+import { useCreateFileMutation } from '../../store/files.api';
 
 /* eslint-disable-next-line */
 export interface NewFileViewProps {}
 
-const StyledNewFileView = styled.div`
-  color: pink;
-`;
+const StyledNewFileView = styled.div``;
 
 export function NewFileView(props: NewFileViewProps) {
+  const [path, setPath] = useState('');
+  const [content, setContent] = useState('');
+
+  const [createFile] = useCreateFileMutation();
+
+  const isButtonDisabled = useMemo(() => {
+    return !path && !content;
+  }, [content, path]);
+
+  const missingDataTooltipTitle = useMemo(() => {
+    if (!path && !content) {
+      return 'Missing content and file path';
+    }
+    if (!content) {
+      return 'Missing content';
+    }
+    if (!path) {
+      return 'Missing file path';
+    }
+
+    return null;
+  }, [content, path]);
+
   return (
     <StyledNewFileView>
-      <h1>Welcome to NewFileView!</h1>
+      <FilePathTitle
+        path={path}
+        onChange={(value) => {
+          // dispatch(editorSlice.actions.update({ path: value }));
+          setPath(value);
+        }}
+        onSave={() => {
+          if (!path || !content) {
+            return;
+          }
+
+          if (isButtonDisabled) {
+            return;
+          }
+
+          createFile({ content: content, filename: path });
+        }}
+        iconTooltip={missingDataTooltipTitle ?? ''}
+      />
+
+      <MdEditor
+        content={content}
+        onChange={(value) => {
+          setContent(value);
+        }}
+      />
     </StyledNewFileView>
   );
 }
