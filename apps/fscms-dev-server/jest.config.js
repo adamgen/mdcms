82564fda1098b2c9bf60407f00192c63c0785b22
Adam <adamgensh@gmail.com>
2022-03-28 17:39:47 +0300

    nx mv file-server fscms-dev-server

--- apps/fscms-dev-server/jest.config.js
+++ apps/fscms-dev-server/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'file-server',
+  displayName: 'fscms-dev-server',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/file-server',
+  coverageDirectory: '../../coverage/apps/fscms-dev-server',
 };
