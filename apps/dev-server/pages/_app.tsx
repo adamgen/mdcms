b8b4ec9da244e9a52f5b0f1990fb926109dd21cb
Adam <adamgensh@gmail.com>
2022-03-31 23:22:23 +0300

    Add store to the next app

--- apps/dev-server/pages/_app.tsx
+++ apps/dev-server/pages/_app.tsx
@@ -1,17 +1,35 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { EDITOR_FEATURE_KEY, editorReducer, filesApi } from '@md-cms/store';
+
+const store = configureStore({
+  reducer: {
+    [EDITOR_FEATURE_KEY]: editorReducer,
+    [filesApi.reducerPath]: filesApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(filesApi.middleware),
+  devTools: process.env['NODE_ENV'] !== 'production',
+  enhancers: [],
+});
+
+if (typeof window !== 'undefined' && (window as any)?.Cypress) {
+  (window as any).store = store;
+}
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>Welcome to dev-server!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
-    </>
+    </Provider>
   );
 }
 
