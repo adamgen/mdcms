bda79785b7b1e09b1be61fb3ca6995ed43437206
Adam <adamgensh@gmail.com>
2022-06-10 15:07:18 +0300

    Refactor filesRouter.spec.ts code style

--- libs/file-server/src/app/filesRouter.spec.ts
+++ libs/file-server/src/app/filesRouter.spec.ts
@@ -4,69 +4,55 @@ import * as path from 'path';
 import * as fs from 'fs';
 import { initFilesTest } from './init-files-test';
 
-const testRoute = async (
-  route: string,
-  test: (response: request.Response) => unknown
-) => {
-  const response = await request(app).get(route);
-  test(response);
-};
+const getFilePath = (...pathParts: string[]) =>
+  path.join(process.env['FILES_SERVER_BASE_PATH'], ...pathParts);
 
 describe('GET files', () => {
   initFilesTest(__dirname);
 
   it('should return a list of files when existing', async () => {
-    await testRoute('/api/files', (response) => {
-      expect(response.statusCode).toBe(200);
-      expect(response.body.sort()).toEqual([
+    await request(app)
+      .get('/api/files')
+      .expect(200, [
         { type: 'file', path: 'a-great-post.md' },
         { type: 'directory', path: 'category' },
         { type: 'file', path: 'index.md' },
         { type: 'file', path: 'my-post.md' },
       ]);
-    });
   });
 
   it('should return a list of files from a subfolder', async () => {
-    await testRoute('/api/files/category', (response) => {
-      expect(response.statusCode).toBe(200);
-      expect(response.body.sort()).toEqual([
+    await request(app)
+      .get('/api/files/category')
+      .expect(200, [
         { type: 'file', path: 'index.md' },
         { type: 'directory', path: 'sub-category' },
       ]);
-    });
   });
 
   it('should return a list of files from a sub-subfolder', async () => {
-    await testRoute('/api/files/category/sub-category', (response) => {
-      expect(response.statusCode).toBe(200);
-      expect(response.body.sort()).toEqual([
-        { type: 'file', path: 'index.md' },
-      ]);
-    });
+    await request(app)
+      .get('/api/files/category/sub-category')
+      .expect(200, [{ type: 'file', path: 'index.md' }]);
   });
 
   it('should return the contents of a file by param', async () => {
-    await testRoute('/api/files/index.md', (response) => {
-      expect(response.body).toMatchInlineSnapshot(`
-        "# Im the index
-        "
-      `);
-    });
+    await request(app)
+      .get('/api/files/index.md')
+      .expect(200, '"# Im the index\\n"');
   });
 });
 
-const getfile = (filePath: string) => {
-  return fs
-    .readFileSync(path.join(process.env['FILES_SERVER_BASE_PATH'], filePath))
-    .toString();
+const getfilePath = (filePath: string) => {
+  return fs.readFileSync(getFilePath(filePath)).toString();
 };
 
 describe('Upsert post/put files', () => {
   initFilesTest(__dirname);
   it('should fail to write to a file with missing body', async () => {
-    const response = await request(app).post('/api/files/new-file.md');
-    expect(response.statusCode).toBe(400);
+    const response = await request(app)
+      .post('/api/files/new-file.md')
+      .expect(400);
     expect(response.body).toBe('Required body not provided.');
   });
 
@@ -74,52 +60,54 @@ describe('Upsert post/put files', () => {
     const response = await request(app)
       .post('/api/files/index.md')
       .send({ content: '# new index file header!' })
-      .set('Accept', 'application/json');
-    expect(response.statusCode).toBe(201);
+      .set('Accept', 'application/json')
+      .expect(201);
     expect(response.body).toMatchInlineSnapshot(
       `"Stored file to process.env['FILES_SERVER_BASE_PATH']/index.md"`
     );
 
-    expect(getfile('index.md')).toEqual('# new index file header!');
+    expect(getfilePath('index.md')).toEqual('# new index file header!');
   });
 
   it('should succeed writing to an existing subfolder file path', async () => {
     const response = await request(app)
       .post('/api/files/category/index.md')
       .send({ content: '# new index file header!' })
-      .set('Accept', 'application/json');
-    expect(response.statusCode).toBe(201);
+      .set('Accept', 'application/json')
+      .expect(201);
     expect(response.body).toMatchInlineSnapshot(
       `"Stored file to process.env['FILES_SERVER_BASE_PATH']/category/index.md"`
     );
 
-    expect(getfile('category/index.md')).toEqual('# new index file header!');
+    expect(getfilePath('category/index.md')).toEqual(
+      '# new index file header!'
+    );
   });
 
   it('should write to a new file', async () => {
     const response = await request(app)
       .post('/api/files/new-file.md')
       .send({ content: '# new random file header!' })
-      .set('Accept', 'application/json');
-    expect(response.statusCode).toBe(201);
+      .set('Accept', 'application/json')
+      .expect(201);
     expect(response.body).toMatchInlineSnapshot(
       `"Stored file to process.env['FILES_SERVER_BASE_PATH']/new-file.md"`
     );
 
-    expect(getfile('new-file.md')).toEqual('# new random file header!');
+    expect(getfilePath('new-file.md')).toEqual('# new random file header!');
   });
 
   it('should write to a non existing path', async () => {
     const response = await request(app)
       .post('/api/files/subfolder/sub-subfolder/new-file.md')
       .send({ content: '# new random file header!' })
-      .set('Accept', 'application/json');
-    expect(response.statusCode).toBe(201);
+      .set('Accept', 'application/json')
+      .expect(201);
     expect(response.body).toMatchInlineSnapshot(
       `"Stored file to process.env['FILES_SERVER_BASE_PATH']/subfolder/sub-subfolder/new-file.md"`
     );
 
-    expect(getfile('subfolder/sub-subfolder/new-file.md')).toEqual(
+    expect(getfilePath('subfolder/sub-subfolder/new-file.md')).toEqual(
       '# new random file header!'
     );
   });
@@ -128,62 +116,48 @@ describe('Upsert post/put files', () => {
 describe('Delete files', () => {
   initFilesTest(__dirname);
   it('should fail to delete non existing files', async () => {
-    const response = await request(app).delete('/api/files/new-file.md');
-    expect(response.statusCode).toBe(400);
-    expect(response.body).toBe('Delete on non existing files.');
+    await request(app)
+      .delete('/api/files/new-file.md')
+      .expect(400, '"Delete on non existing files."');
   });
 
   it('should delete existing files', async () => {
-    const indexFilePath = path.join(
-      process.env['FILES_SERVER_BASE_PATH'],
-      'index.md'
-    );
+    const indexFilePath = getFilePath('index.md');
     expect(fs.existsSync(indexFilePath)).toBeTruthy();
 
-    const response = await request(app).delete('/api/files/index.md');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toBe('Deleted');
+    await request(app).delete('/api/files/index.md').expect(200, '"Deleted"');
     expect(fs.existsSync(indexFilePath)).toBeFalsy();
   });
 
   it('should delete existing subfolder files', async () => {
-    const indexFilePath = path.join(
-      process.env['FILES_SERVER_BASE_PATH'],
-      'category/index.md'
-    );
+    const indexFilePath = getFilePath('category/index.md');
     expect(fs.existsSync(indexFilePath)).toBeTruthy();
 
-    const response = await request(app).delete('/api/files/category/index.md');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toBe('Deleted');
+    await request(app)
+      .delete('/api/files/category/index.md')
+      .expect(200, '"Deleted"');
     expect(fs.existsSync(indexFilePath)).toBeFalsy();
   });
 
   // TODO add multer to support file uploads
   it.skip('should post files', async () => {
     const buf = Buffer.from('');
-    //
-    const response = await request(app)
+    await request(app)
       .post('/api/files/my.jpg')
       .set('Accept', 'multipart/form-data')
-      .attach('', buf);
-
-    expect(response.statusCode).toBe(200);
+      .attach('', buf)
+      .expect(200);
   });
-
-  const getFilePath = (...pathParts: string[]) =>
-    path.join(process.env['FILES_SERVER_BASE_PATH'], ...pathParts);
 
   const moveFile = async (source: string, dest: string) => {
     const sourceFilePath = getFilePath(source);
     const destFilePath = getFilePath(dest);
 
     expect(fs.existsSync(destFilePath)).toBeFalsy();
-    const response = await request(app)
+    await request(app)
       .put(`/api/files/${source}`)
-      .send({ filePath: dest });
-
-    expect(response.statusCode).toBe(200);
+      .send({ filePath: dest })
+      .expect(200);
     expect(fs.existsSync(sourceFilePath)).toBeFalsy();
     expect(fs.existsSync(destFilePath)).toBeTruthy();
     expect(fs.readFileSync(destFilePath).toString()).toEqual(
