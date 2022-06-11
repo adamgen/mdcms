02378c57e559ca0d2adb67e8318ce2af1b735a90
Adam <adamgensh@gmail.com>
2022-06-11 21:38:59 +0300

    Support running tests in parallel with temp ids

--- libs/file-server/src/app/init-files-test.ts
+++ libs/file-server/src/app/init-files-test.ts
@@ -3,9 +3,22 @@
 import * as path from 'path';
 import * as fs from 'fs-extra';
 
+function makeid(length) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 export const initFilesTest = (dir: string) => {
   let env = process.env;
-  const tempTestDir = path.join(dir, '__test-temp-data__');
+  const uid = makeid(7);
+  const tempTestDir = path.join(dir, `__test-temp-data__${uid}`);
   beforeEach(() => {
     env = process.env;
     process.env['FILES_SERVER_BASE_PATH'] = tempTestDir;
