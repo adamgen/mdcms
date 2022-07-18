258998427003e357a5716a954f73a1ce46fd9811
Adam <adamgensh@gmail.com>
2022-07-18 09:55:00 +0300

    Cleanup dev-server

--- apps/dev-server/pages/_app.tsx
+++ apps/dev-server/pages/_app.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { EDITOR_FEATURE_KEY, editorReducer, filesApi } from '@md-cms/ui-store';
-import { AppLayout } from '@md-cms/ui-react/src/app/app-layout/app-layout';
+import { AppLayout } from '@md-cms/ui-react';
 
 const store = configureStore({
   reducer: {
--- libs/ui/react/src/index.ts
+++ libs/ui/react/src/index.ts
@@ -1,1 +1,2 @@
 export { App } from './app/app';
+export { AppLayout } from './app/app-layout/app-layout';
